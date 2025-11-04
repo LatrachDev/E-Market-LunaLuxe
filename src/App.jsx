0fc35ec78ad152820361ecd9395ac7528838fdec
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-04 17:29:20 +0100

    LUN-3 Create components for the home page | install react router dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import './index.css';
+import RoutesList from './Routes/Routes';
+
+
+function App() {
+
+  return (
+    <>
+
+      <RoutesList />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import IndexPage from "../pages/Index/IndexPage";
+import ProductsPage from "../pages/Products/ProductsPage";
+import NotFound from "../pages/Error/NotFound";
+import LoginPage from '../pages/Auth/LoginPage';
+
+export default function RoutesList() {
+    return (
+        <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path={'*'} element={<NotFound />} />
+        </Routes>
+    )
+}
