8e5aa065d5761914ff9ec6b8e4afbd6b201231ee
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-04 17:27:57 +0100

    LUN-1 Install ReactJs template and tailwindcss && configiration of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+        montserrat: ['"Montserrat"', 'sans-serif'], 
+      },
+      colors: {
+        brandBrown: "#633F3B",
+        brandRed: "#B5483A",
+        brandSwhite: "#f1f1f1",
+        brandWhite: "#fffaf9",
+        l_black: "#1f1f1f",
+      },
+    },
+  },
+  plugins: [],
+};
