a674a38a8d04ca3cc9b8ebb1a4702ed1cc51ee98
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-05 19:12:22 +0100

    LUN-3 Create components for the home page

--- src/pages/Index/IndexPage.jsx
+++ src/pages/Index/IndexPage.jsx
@@ -2,6 +2,9 @@ import Header from '../../components/Home/Header';
 import NavBar from '../../components/Layouts/NavBar';
 import LoginPage from '../Auth/LoginPage';
 import HomeProducts from '../../components/Home/HomeProducts';
+import Discover from '../../components/Home/Discover';
+import History from '../../components/Home/History';
+import Routine from '../../components/Home/Routine';
 
 export default function IndexPage()
 {
@@ -10,6 +13,9 @@ export default function IndexPage()
             <NavBar />
             <Header />
             <HomeProducts />
+            <Discover />
+            <History />
+            <Routine />
         </>
     )
 }
